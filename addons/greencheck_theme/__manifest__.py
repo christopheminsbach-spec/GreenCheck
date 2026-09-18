1a89215286da234dd878bc1e9d91d3219db71878
Angus <angus57280@gmail.com>
2026-09-18 09:03:57 +0200

    feat: add GreenCheck footer

--- addons/greencheck_theme/__manifest__.py
+++ addons/greencheck_theme/__manifest__.py
@@ -19,12 +19,14 @@
         "views/homepage.xml",
         "views/header.xml",
         "views/diagnostic.xml",
+        "views/footer.xml",
     ],
 
     "assets": {
         "web.assets_frontend": [
             "greencheck_theme/static/src/css/greencheck.scss",
             "greencheck_theme/static/src/css/diagnostic.scss",
+            "greencheck_theme/static/src/css/footer.scss",
         ],
     },
 
